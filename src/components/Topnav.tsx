2d8121cab5b2208427851e58903d99d3d7344c2b
kgruca <kgruca21@gmail.com>
2023-05-15 21:30:29 -0700

    topnav stage 1 complete

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -4,13 +4,15 @@ import Logo from "../../public/bronze.png";
 
 const TopNavigation = () => (
   <nav
-    className="flex flex-row sticky top-0 flex-nowrap items-center justify-between 
-    bg-slate-600 h-14"
+    className="flex w-full flex-row sticky top-0 flex-nowrap items-center justify-between 
+    h-14"
   >
-    <div className="flex w-full">
-      <Link href="/" className="flex flex-row float-left ml-2">
-        <Image src={Logo} width={50} height={50} alt="logo" />
-      </Link>
+    <Link href="/" className="w-full ml-2">
+      <Image src={Logo} width={50} height={50} alt="logo" />
+    </Link>
+    <div className="flex flex-row w-full justify-end gap-4 mr-4">
+      <Link href="/about">About</Link>
+      <Link href="/login">Log In</Link>
     </div>
   </nav>
 );
